61422cb05cb59339fe7834b2305c7c023268b6c8
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-15 09:57:20 -0400

    set up context into Budget.js and ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,18 @@
-import React from "react"
-import ExpenseItem from "./ExpenseItem"
+import React, { useContext } from "react";
+import ExpenseItem from "./ExpenseItem";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseList = () => {
-    const expenses = [
-        { id: 1231, name: "Shopping", cost: 50},
-        { id: 1231, name: "Holiday", cost: 300},
-        { id: 1231, name: "Transportation", cost: 70},
-        { id: 1231, name: "Fuel", cost: 40},
-        { id: 1231, name: "Child Care", cost: 500},
-    ];
+    const {expenses} = useContext(AppContext)
+
+    // const expenses = [
+
+    //     { id: 1231, name: "Shopping", cost: 50},
+    //     { id: 1231, name: "Holiday", cost: 300},
+    //     { id: 1231, name: "Transportation", cost: 70},
+    //     { id: 1231, name: "Fuel", cost: 40},
+    //     { id: 1231, name: "Child Care", cost: 500},
+    // ];
     return (
         <ul className="list-group">
             {expenses.map((expense)=> (
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -12,7 +12,8 @@ const initialState = {
     expenses: [
         {id: 12, name: "shopping", cost: 40 },
         {id: 13, name: "holiday", cost: 400 },
-
+        {id: 14, name: "car service", cost: 50 }
+    
     ]
 }
 
