641d3ca23e408884c086cc24d82297467fef384c
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-16 12:12:55 -0400

    Added SET_BUDGET to AppContext

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,33 @@
-import React, { useContext} from "react";
-import { AppContext } from "../context/AppContext";
+import React, { useState, useContext } from 'react';
+import ViewBudget from './ViewBudget.js';
+import EditBudget from './EditBudget.js';
+import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext)
+	const { budget, dispatch } = useContext(AppContext);
+	const [isEditing, setIsEditing] = useState(false);
 
-    return (
-        <div className = "alert alert-secondary">
-            <span>Budget: ${budget}</span>
-        </div>
-    )
-}
+	const handleEditClick = () => {
+		setIsEditing(true);
+	};
+
+	const handleSaveClick = value => {
+		dispatch({
+			type: 'SET_BUDGET',
+			payload: value,
+		});
+		setIsEditing(false);
+	};
+
+	return (
+		<div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
+			{isEditing ? (
+				<EditBudget handleSaveClick={handleSaveClick} budget={budget} />
+			) : (
+				<ViewBudget handleEditClick={handleEditClick} budget={budget} />
+			)}
+		</div>
+	);
+};
 
 export default Budget;
